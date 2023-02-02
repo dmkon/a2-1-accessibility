8d51dea2f567e4fab424d436ebc0f43715865967
dmkon <dkondramachine@league.com>
2023-02-02 09:14:31 -0500

    Initial Commit

---
+++ a2-1-accessibility/a2_1_accessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  a2_1_accessibilityApp.swift
+//  a2-1-accessibility
+//
+//  Created by dmitri.kondramachine on 2023-02-02.
+//
+
+import SwiftUI
+
+@main
+struct a2_1_accessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
