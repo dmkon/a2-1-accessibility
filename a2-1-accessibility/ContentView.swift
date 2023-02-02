7782d47000c1f8ae72f1a8744a4c2eabe2a79c91
dmkon <dkondramachine@league.com>
2023-02-02 10:38:17 -0500

    Accessibility not set - default behaviour.

--- a2-1-accessibility/ContentView.swift
+++ a2-1-accessibility/ContentView.swift
@@ -10,10 +10,42 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Button {
+                    //none
+                } label: {
+                    Image(systemName: "xmark")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                }
+                Spacer()
+            }
+            
+            Spacer()
+            Image("Journey")
+            
+            Text("Build healthy new habits")
+                .font(.title)
+                .bold()
+            
+            Text("Discover a wide selection of programs to help you reach your health goals")
+                .font(.title2)
+            
+            Spacer()
+            
+            Divider().frame(maxWidth: .infinity)
+            HStack {
+                Spacer()
+                Button {
+                    //none
+                } label: {
+                    Image(systemName: "arrow.right.circle")
+                        .resizable()
+                        .frame(width: 48.0, height: 48.0)
+                        .foregroundColor(.accentColor)
+                }
+            }
+        
         }
         .padding()
     }
